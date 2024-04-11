837743bdd9b2e9c93c7f27694128d3eeba614d7f
ragulraj <rraj9350@gmail.com>
2024-04-11 11:13:29 +0530

    Integrated Tailwind css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      dxd
+    <div className="App text-center flex justify-center items-center h-10 w-full text-blue-600 font-medium">
+      Huddle Now Extension
     </div>
   );
 }
